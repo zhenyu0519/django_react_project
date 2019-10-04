3b1ca35ac66582ea396cd577497de4f0c4472d71
Jeffery Zhang <zhenyu051988@gmail.com>
2019-10-04 16:38:37 -0700

    init the react env as front end

--- leadmanager_project/lead_app/models.py
+++ leadmanager_project/lead_app/models.py
@@ -4,6 +4,8 @@ from django.db import models
 # Create your models here.
 class Lead(models.Model):
     name = models.CharField(max_length=100)
+    position = models.CharField(max_length=100, blank=True)
     email = models.EmailField(max_length=100, unique=True)
+    group = models.PositiveIntegerField(default=1)
     message = models.CharField(max_length=500, blank=True)
     created_at = models.DateTimeField(auto_now=True)
--- leadmanager_project/leadmanager_project/settings.py
+++ leadmanager_project/leadmanager_project/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'lead_app',
-    'rest_framework'
+    'rest_framework',
+    'front_app'
 ]
 
 MIDDLEWARE = [
