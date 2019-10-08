b432a5547c0b2b06112b841f65b873c6f2fa03a1
Jeffery Zhang <zhenyu051988@gmail.com>
2019-10-07 17:32:48 -0700

    login and logout auth added

--- leadmanager_project/accounts_app/serializers.py
+++ leadmanager_project/accounts_app/serializers.py
@@ -21,8 +21,16 @@ class RegisterSerializer(serializers.ModelSerializer):
         user = User.objects.create_user(validated_data['username'],
                                         validated_data['email'],
                                         validated_data['password'])
-
         return user
 
 
 #login serializer
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
+
+    def validate(self, data):
+        user = authenticate(**data)
+        if user and user.is_active:
+            return user
+        raise serializers.ValidationError("Incorrect Credentials")
